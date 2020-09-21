e82ffc80108e8383b6a6199ab59be53db4308ee7
TheoJ <theojolliffe@gmail.com>
2020-09-21 22:32:15 +0100

    Meta 9

--- seeablestats/urls.py
+++ seeablestats/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('ndombele', views.ndombele, name='ndombele'),
     path('soton', views.soton, name='soton'),
-    path('SpursVsSoton2', views.spursVSoton, name='spursvssoton2'),
+    path('SpursVSoton', views.spursVSoton, name='SpursVSoton'),
     path('spurs', views.spurs, name='spurs'),
     path('teams', views.teams, name='teams'),
     path('', views.home, name='home'),
